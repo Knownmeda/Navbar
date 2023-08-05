33cd6289f0f28d8d63a4b0b8ba7db33e6fac88d4
Rizcky <rizkyricky15@gmail.com>
2023-08-05 12:17:24 +0700

    permulaan

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector(".hamburger");
+const navBar = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navBar.classList.toggle("active");
+})
