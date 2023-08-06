f291b4f09108eab684d3a00249503246a1ad9821
Rizcky <rizkyricky15@gmail.com>
2023-08-06 09:52:20 +0700

    dropdown

--- script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const navBar = document.querySelector(".navbar");
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navBar.classList.toggle("active");
-})
